3f9f00c5f2e2b7b4fd6e3cb7b6dcaaf93f13e630
Antrax <antraxsec@gmail.com>
2024-06-13 12:16:29 -0600

    hardy 13/06 12:16

--- resources/src/containers/layouts/largeSidebar/TopNav.vue
+++ resources/src/containers/layouts/largeSidebar/TopNav.vue
@@ -24,7 +24,7 @@
       </button>
        <router-link v-if="currentUserPermissions && currentUserPermissions.includes('Pos_view')"
         class="btn btn-outline-primary tn-sm btn-rounded" to="/app/sales/store">
-        <span class="ul-btn__text ml-1">POS</span>
+        <span class="ul-btn__text ml-1">+ VENTAS</span>
       </router-link>
       <!-- <router-link v-if="currentUserPermissions && currentUserPermissions.includes('Pos_view')"
         class="btn btn-outline-primary tn-sm btn-rounded" to="/app/pos">
