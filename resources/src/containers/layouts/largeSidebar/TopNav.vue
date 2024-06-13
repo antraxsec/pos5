59c71d5839228ba4942854affa503340fa350586
Antrax <antraxsec@gmail.com>
2024-06-13 12:08:27 -0600

    hardy 13/06 08:16 ok

--- resources/src/containers/layouts/largeSidebar/TopNav.vue
+++ resources/src/containers/layouts/largeSidebar/TopNav.vue
@@ -22,10 +22,14 @@
       <button @click="lista" class="btn btn-outline-danger tn-sm btn-rounded">
         <span class="ul-btn__text ml-1">Lista</span>
       </button>
-      <router-link v-if="currentUserPermissions && currentUserPermissions.includes('Pos_view')"
-        class="btn btn-outline-primary tn-sm btn-rounded" to="/app/pos">
+       <router-link v-if="currentUserPermissions && currentUserPermissions.includes('Pos_view')"
+        class="btn btn-outline-primary tn-sm btn-rounded" to="/app/sales/store">
         <span class="ul-btn__text ml-1">POS</span>
       </router-link>
+      <!-- <router-link v-if="currentUserPermissions && currentUserPermissions.includes('Pos_view')"
+        class="btn btn-outline-primary tn-sm btn-rounded" to="/app/pos">
+        <span class="ul-btn__text ml-1">POS</span>
+      </router-link> -->
       <!-- Full screen toggle -->
       <i class="i-Full-Screen header-icon d-none d-sm-inline-block" @click="handleFullScreen"></i>
       <!-- Grid menu Dropdown -->
